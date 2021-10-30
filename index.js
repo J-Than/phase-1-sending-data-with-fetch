5c31b93c18662555246a94fb01729b9b10d89af4
Jarod Wilson <jaroddwilson@gmail.com>
2021-10-30 17:56:54 -0400

    commit tree 787918547066e600dbc4be5c947ca9c7ef0b03e2

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Add your code here
+function submitData(userName, userEmail) {
+  const newUser = {
+    name: userName,
+    email: userEmail
+  }
+  return fetch('http://localhost:3000/users', {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json",
+    },
+    body: JSON.stringify(newUser),
+  })
+  .then (resp => resp.json())
+  .then (data => {
+    let p = document.createElement('p');
+    p.textContent = data.id;
+    document.querySelector('script').appendChild(p);
+  })
+  .catch (error => {
+    let p = document.createElement('p');
+    p.textContent = error.message;
+    document.querySelector('script').appendChild(p);
+  })
+}
